83b35abb3597b6980b079910c62b57c7802490ad
Carl J Nash <carl@carljnash.com>
2018-02-25 21:56:15 +0000

    Add base navigation and view controllers and list and map view controllers

---
+++ Recycle Now/UI/Map/RecyclePointMapNavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  RecyclePointMapNavigationController.swift
+//  Recycle Now
+//
+//  Created by Carl on 25/02/2018.
+//  Copyright © 2018 CarlJNash. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecyclePointMapNavigationController: BaseNavigationController {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+    }
+}
