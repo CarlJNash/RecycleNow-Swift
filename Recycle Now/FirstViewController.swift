6021d34a3bc0d35d80e8d451b9a9de810452a4a4
Carl J Nash <carl@carljnash.com>
2018-02-25 08:12:21 +0000

    Initial Commit

---
+++ Recycle Now/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Recycle Now
+//
+//  Created by Carl on 25/02/2018.
+//  Copyright © 2018 CarlJNash. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
