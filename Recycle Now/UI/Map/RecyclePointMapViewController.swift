7fadcae35e23d94c1542b4ec6e3f0bfeb4b58bea
Carl J Nash <carl@carljnash.com>
2018-03-03 22:54:42 +0000

    Download recycle points for the map view

--- Recycle Now/UI/Map/RecyclePointMapViewController.swift
+++ Recycle Now/UI/Map/RecyclePointMapViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class RecyclePointMapViewController: BaseViewController {
 
+    // ===============================================
+    // MARK: - Properties
+    // ===============================================
+    
+    var recycleItems: RecycleItems? {
+        didSet {
+            // TODO: update map pins
+        }
+    }
+    
+    
+    
+    // ===============================================
+    // MARK: - UIViewController Lifecycle
+    // ===============================================
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fetchRecyclePoints()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    // ===============================================
+    // MARK: - Private
+    // ===============================================
+    
+    fileprivate func fetchRecyclePoints() {
+        // TODO: make location not hardcoded!
+        let config = URLSessionDataTaskConfig.RecycleLocation.init(location: "EH35BW")
+        NetworkClient.shared.dataTaskWith(config: config, decodableType: RecycleItems.self, successBlock: { (response) in
+            
+            if let recycleItems = response.decodableResponse as? RecycleItems {
+                self.recycleItems = recycleItems
+            }
+        }) { (failure) in
+            // TODO: Handle error
+            print(failure.error as Any)
+        }
+    }
 }
 
